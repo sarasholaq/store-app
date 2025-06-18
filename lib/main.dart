fa42050a02161e122f45ebfb4c17084f8ded5a07
sarasholaq <sarasholaq@gmail.com>
2025-06-18 16:23:15 +0300

    start app

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:store/store_app.dart';
+
+void main() {
+  runApp(const StoreApp());
+}
+
+
+
---
+++ lib/store_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class StoreApp extends StatelessWidget {
+  const StoreApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Store App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Store'),
+        ),
+      ),
+    );
+  }
+}
